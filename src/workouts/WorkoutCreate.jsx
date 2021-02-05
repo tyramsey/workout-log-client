d607b7eed1a8a6afb2ca333aa7c4fd64b3d4b7f8
Ty Ramsey <tydram@gmail.com>
2021-02-05 13:18:14 -0500

    first commit

---
+++ src/workouts/WorkoutCreate.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+const WorkoutCreate = (props) => {
+  const [description, setDescription] = useState("");
+  const [definition, setDefinition] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/log/log/", {
+      method: "POST",
+      body: JSON.stringify({
+        log: {
+          description: description,
+          definition: definition,
+          result: result,
+        },
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": props.token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((logData) => {
+        console.log(logData);
+        setDescription("");
+        setDefinition("");
+        setResult("");
+        props.fetchWorkouts();
+      });
+  };
+
+  return (
+    <>
+      <h3>Log a Workout</h3>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="description" />
+          <Input
+            name="description"
+            placeholder='Description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="definiton" />
+          <Input
+            type='select'
+            name="definition"
+            placeholder='Definition'
+            value={definition}
+            onChange={(e) => setDefinition(e.target.value)}>
+            <option value="Time">Time</option>
+            <option value="Weight">Weight</option>
+            <option value="Distance">Distance</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="result" />
+          <Input
+            name="result"
+            placeholder='Result'
+            value={result}
+            onChange={(e) => setResult(e.target.value)}
+          />
+        </FormGroup>
+        <Button type="submit">Click to Submit</Button>
+      </Form>
+    </>
+  );
+};
+
+export default WorkoutCreate;
---
+++ src/workouts/WorkoutEdit.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+} from "reactstrap";
+
+const WorkoutEdit = (props) => {
+  const [editDesc, setEditDesc] = useState(props.workoutToUpdate.description);
+  const [editDef, setEditDef] = useState(props.workoutToUpdate.definition);
+  const [editRes, setEditRes] = useState(props.workoutToUpdate.result);
+  const workoutUpdate = (event, workout) => {
+    event.preventDefault();
+    fetch(`http://localhost:3000/log/log/${props.workoutToUpdate.id}`, {
+      method: "PUT",
+      body: JSON.stringify({log: { description: editDesc, definition: editDef, result: editRes }}),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        'Authorization': props.token,
+      }),
+    }).then((res) => {
+      props.fetchWorkouts();
+      props.updateOff();
+    });
+  };
+
+  return (
+  <Modal isOpen={true}>
+  <ModalHeader>Log a Workout</ModalHeader>
+  <ModalBody>
+      <Form onSubmit={workoutUpdate}>
+        <FormGroup>
+            <Label htmlFor='result'>Edit Result:</Label>
+            <Input name='result' value={editRes} onChange={(e) => setEditRes(e.target.value)}/>
+        </FormGroup>
+        <FormGroup>
+            <Label htmlFor='description'>Edit Description:</Label>
+            <Input name='description' value={editDesc} onChange={(e) => setEditDesc(e.target.value)}/>
+        </FormGroup>
+        <FormGroup>
+            <Label htmlFor='definition'>Edit Definition:</Label>
+            <Input type='select' name='definition' value={editDef} onChange={(e) => setEditDef(e.target.value)}>
+                <option></option>
+                <option value='time'>Time</option>
+                <option value='weight'>Weight</option>
+                <option value='distance'>Distance</option>
+              </Input>
+        </FormGroup>
+        <Button type='submit'>Update the Workout</Button>
+      </Form>
+  </ModalBody>
+  </Modal>
+  )
+  };
+
+export default WorkoutEdit;
